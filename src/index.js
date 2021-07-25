abbb13bac67bea839a05903ee89d3970d1afe050
kanishths <kanishthasiyaram15@gmail.com>
2021-07-25 21:42:45 +0530

    Auth completed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const Task = require("./models/task");
 const { ReplSet } = require("mongodb");
 
 const userRouter = require("./routers/user");
-const taskRouter = require("./routers/tasks");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -30,17 +30,5 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-const jwt = require("jsonwebtoken");
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "thisismynewtoy", {
-    expiresIn: "7 days",
-  });
-  // console.log(token);
-
-  const data = jwt.verify(token, "thisismynewtoy");
-  // console.log(data);
-};
-myFunction();
 
 //  M:/mongodb/bin/mongod.exe --dbpath=M:/mongodb-data
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -51,6 +52,20 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "Thisismynewtoy");
@@ -72,12 +87,19 @@ userSchema.statics.findByCredentials = async (email, password) => {
   return user;
 };
 
-//Hash plain text pw
+//Hash plain text password before saving
 userSchema.pre("save", async function (next) {
   const user = this;
   if (user.isModified("password")) {
     user.password = await bcrypt.hash(user.password, 8);
   }
+  next();
+});
+
+//Deleting User task when user is removed
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Task.deleteMany({ owner: user._id });
   next();
 });
 
