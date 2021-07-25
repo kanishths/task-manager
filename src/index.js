66a26ba382fca74ec58427d14c1142bf94a8a976
kanishths <kanishthasiyaram15@gmail.com>
2021-07-25 13:58:11 +0530

    Auth added

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ const taskRouter = require("./routers/tasks");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -18,5 +23,17 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
+
+const jwt = require("jsonwebtoken");
+const myFunction = async () => {
+  const token = jwt.sign({ _id: "abc123" }, "thisismynewtoy", {
+    expiresIn: "7 days",
+  });
+  console.log(token);
+
+  const data = jwt.verify(token, "thisismynewtoy");
+  console.log(data);
+};
+myFunction();
 
 //  M:/mongodb/bin/mongod.exe --dbpath=M:/mongodb-data
--- src/routers/tasks.js
+++ src/routers/tasks.js
@@ -48,16 +48,17 @@ router.patch("/tasks/:id", async (req, res) => {
   const _id = req.params.id;
 
   try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, {
-      new: true,
-      isValidOperation: true,
-    });
+    const task = await Task.findById(_id);
+
+    updates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
+
     if (!task) {
       return res.status(404).send();
     }
     res.status(200).send(task);
   } catch (e) {
-    res.status(400).send();
+    res.status(400).send(e);
   }
 });
 
