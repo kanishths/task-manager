6ffe40ac02a7765947136bb478b8e903e59bf3a3
kanishths <kanishthasiyaram15@gmail.com>
2021-07-26 21:08:26 +0530

    avatar/profile pic added

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
-const { response } = require("express");
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/user");
-const Task = require("./models/task");
-const { ReplSet } = require("mongodb");
-
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("Get requeets are disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send("Under Maintanence");
-// });
 
 app.use(express.json());
 app.use(userRouter);
